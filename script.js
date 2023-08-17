c0dde1b4fbd24f6307cc4bd7190cb5b08237338f
Lukesh Bahurupi <72219338+lukeshbahurupi@users.noreply.github.com>
2023-08-17 22:50:44 +0530

    c

--- script.js
+++ script.js
@@ -66,3 +66,7 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
 });
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') check();
+  console.log(e.key);
+});
